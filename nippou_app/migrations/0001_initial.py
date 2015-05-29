6e1bfb47b933b7eed6ac8e899f122af0afd2eaf4
tie304184 <ykoga.kyutech@gmail.com>
2015-05-29 10:48:47 +0900

    modified README.md

--- nippou_app/migrations/0001_initial.py
+++ nippou_app/migrations/0001_initial.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.utils.timezone
-import datetime
 import django.contrib.auth.models
 from django.conf import settings
+import django.utils.timezone
+import datetime
 import django.core.validators
 
 
@@ -19,25 +19,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
                 ('password', models.CharField(verbose_name='password', max_length=128)),
                 ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
-                ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
-                ('username', models.CharField(validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], verbose_name='username', error_messages={'unique': 'A user with that username already exists.'}, unique=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=30)),
+                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', default=False, verbose_name='superuser status')),
+                ('username', models.CharField(verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], max_length=30, unique=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', error_messages={'unique': 'A user with that username already exists.'})),
                 ('first_name', models.CharField(verbose_name='first name', blank=True, max_length=30)),
                 ('last_name', models.CharField(verbose_name='last name', blank=True, max_length=30)),
                 ('email', models.EmailField(verbose_name='email address', blank=True, max_length=254)),
-                ('is_staff', models.BooleanField(default=False, verbose_name='staff status', help_text='Designates whether the user can log into this admin site.')),
-                ('is_active', models.BooleanField(default=True, verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
+                ('is_staff', models.BooleanField(help_text='Designates whether the user can log into this admin site.', default=False, verbose_name='staff status')),
+                ('is_active', models.BooleanField(help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', default=True, verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('user_dev', models.CharField(max_length=512)),
-                ('groups', models.ManyToManyField(to='auth.Group', verbose_name='groups', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user')),
-                ('user_permissions', models.ManyToManyField(to='auth.Permission', verbose_name='user permissions', blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user')),
+                ('groups', models.ManyToManyField(verbose_name='groups', related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', blank=True, to='auth.Group')),
+                ('user_permissions', models.ManyToManyField(verbose_name='user permissions', related_query_name='user', help_text='Specific permissions for this user.', related_name='user_set', blank=True, to='auth.Permission')),
             ],
             options={
+                'verbose_name_plural': 'users',
                 'verbose_name': 'user',
                 'abstract': False,
-                'verbose_name_plural': 'users',
             },
             managers=[
                 ('objects', django.contrib.auth.models.UserManager()),
@@ -46,17 +46,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='nippou_data',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
                 ('title', models.CharField(verbose_name='タイトル', max_length=512)),
                 ('text', models.TextField(verbose_name='本文')),
-                ('date', models.DateTimeField(verbose_name='投稿日時', default=datetime.datetime.now)),
+                ('date', models.DateTimeField(default=datetime.datetime.now, verbose_name='投稿日時')),
+                ('open', models.BooleanField(default=False, verbose_name='公開')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
                 ('task_name', models.CharField(verbose_name='タスク名', max_length=512)),
                 ('time_yotei', models.IntegerField(verbose_name='予定時間')),
                 ('time_jitsu', models.IntegerField(verbose_name='実時間')),
